82fad4c0efad53ab436ad7f6e47c310eb24fce8d
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-11-12 22:52:35 +0900

    add: not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button"
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty"
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <Empty className="h-screen flex flex-col items-center justify-center bg-background text-foreground">
+      <EmptyHeader >
+        <EmptyMedia variant="default">
+          <h1 className="text-7xl font-bold text-accent">404</h1>
+        </EmptyMedia>
+        <EmptyTitle className="text-3xl font-semibold text-foreground">Page Not Found</EmptyTitle>
+        <EmptyDescription className="text-muted-foreground text-lg">
+          Looks like this page doesn&apos;t exist. Let me take you back home.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button>
+          <Link
+            href="/"
+            className="font-bold"
+          >
+            Back to Home
+          </Link>
+        </Button>
+      </EmptyContent>
+    </Empty>
+  )
+}
